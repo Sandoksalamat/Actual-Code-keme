a7e5469d974ddec07f5c0fd613b3afbcdb60fb04
raayzell <rhaizelprice2006@gmail.com>
2023-05-20 00:34:04 +0800

    - Linagyon ko content yung Sampaguita & Narra

--- app/src/main/java/com/example/sagisag/Narra.java
+++ app/src/main/java/com/example/sagisag/Narra.java
@@ -2,8 +2,11 @@ package com.example.sagisag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Narra extends AppCompatActivity {
@@ -13,9 +16,29 @@ public class Narra extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_narra);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button leftboton = findViewById(R.id.lbutton);
+        Button rightboton = findViewById(R.id.rbutton);
         tv = (TextView) findViewById(R.id.content_narra);
-        String xd = "known_as_the_national_tree_of_the_philippines_narra_is_a_majestic_reddish_hardwood_tree_reflecting_the_same_resiliency_and_strength_as_that_of_the_filipino_people_historically_on_june_21_1969_the_municipality_of_narra_was_created_by_virtue_of_republic_act_no_5642_signed_by_president_ferdinand_marcos_having_a_title_as_quot_the_rice_granary_of_palawan_quot_municipality_of_narra_is_the_main_rice_producer_of_the_province_of_palawan_additionally_narra_is_a_graceful_tree_with_drooping_branches_that_toss_in_the_trade_winds_short_lived_flowers_usually_appear_in_april_or_may_and_for_a_day_or_two_the_tree_is_a_shimmering_mass_of_fragrant_yellow_blooms_which_then_fall_off_nearly_all_at_once";
+        String xd = "Meaning: \n Known as the National Tree of the Philippines, Narra is a majestic reddish hardwood tree reflecting the same resiliency and strength as that of the Filipino people. \n" +
+                "\n History and Backround: \n On June 21, 1969, the Municipality of Narra was created by virtue of Republic Act No. 5642 signed by President Ferdinand Marcos. Having a title as 'The Rice Granary of Palawan' Municipality of Narra is the main rice producer of the Province of Palawan. \n" +
+                "\n Other info: \n Narra is a graceful tree with drooping branches that toss in the trade winds. Short-lived flowers usually appear in April or May, and for a day or two the tree is a shimmering mass of fragrant yellow blooms, which then fall off nearly all at once. \n";
         tv.setText(xd);
         tv.setMovementMethod(new ScrollingMovementMethod());
+        leftboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(Narra.this,Sampaguita.class);
+                startActivity(next);
+            }
+        });
+
+        rightboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(Narra.this, Agilla.class);
+                startActivity(next);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sagisag/Sampaguita.java
+++ app/src/main/java/com/example/sagisag/Sampaguita.java
@@ -1,12 +1,10 @@
 package com.example.sagisag;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GestureDetectorCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.view.GestureDetector;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,7 +20,9 @@ public class Sampaguita extends AppCompatActivity {
 
         Button boton = findViewById(R.id.button3);
         textview = (TextView) findViewById(R.id.content_Sampag);
-        String xd = "it_was_declared_as_the_national_flower_by_governor_general_frank_murphy_in_1934_in_the_philippines_it_is_unusual_for_someone_to_give_sampaguita_to_another_person_as_a_gift_or_as_a_sign_of_adoration_because_it_has_a_lasting_impression_of_being_solely_offered_to_saints_plus_did_you_know_that_sampaguita_is_not_native_to_the_philippines_and_is_still_not_considered_a_naturalized_species_it_has_a_pantropical_distribution_and_is_commonly_cultivated_in_the_country_for_ornamental_purposes_the_sweet_smelling_flowers_are_often_made_into_garlands_and_perfumery_there_are_more_than_10_species_of_jasminum_in_the_philippines_and_8_species_are_considered_endemic_and_are_not_found_elsewhere_in_the_world";
+        String xd = "Meaning:\n symbolize purity, fidelity, and hope \n " +
+                "\n History and Background: \n It was declared as the national flower by Governor-General Frank Murphy in 1934. In the Philippines, it is unusual for someone to give Sampaguita to another person as a gift or as a sign of adoration because it has a lasting impression of being solely offered to saints.\n " +
+                "\n Other Info:\n Did you know that Sampaguita is not native to the Philippines and is still not considered a naturalized species? It has a pantropical distribution and is commonly cultivated in the country for ornamental purposes. The sweet-smelling flowers are often made into garlands and perfumery. There are more than 10 species of Jasminum in the Philippines and 8 species are considered endemic and are not found elsewhere in the world. \n";
         textview.setText(xd);
         textview.setMovementMethod(new ScrollingMovementMethod());
 
